39997615cf8af2872b99c3d5090ba9591a42528e
Dmitriy-Opria <demoni421@gmail.com>
2017-03-18 14:09:50 +0200

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { appRoutingProviders, routing } from './app.routing';
+import { HomeComponent } from './component/home/home.component';
+import { ProfileComponent } from './component/profile/profile.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule,
+                  routing
+                ],
+  declarations: [ AppComponent,
+                  HomeComponent,
+                  ProfileComponent
+                ],
+  bootstrap:    [ AppComponent ],
+  providers:    [ appRoutingProviders ]
 })
 export class AppModule { }
